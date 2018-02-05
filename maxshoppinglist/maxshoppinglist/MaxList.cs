7994efda9643ed5e2d64d70e82f7293818fcb410
Max Liberzon <maxliberzon@gmail.com>
2018-02-04 17:52:58 -0800

    Started shopping list clone by using only class

---
+++ maxshoppinglist/maxshoppinglist/MaxList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace maxshoppinglist
+{
+    class MaxList
+    {
+        //this class should have an array of items
+        //and a size, to know how many items we have
+        //functions we need:
+        //add function
+        //remove function
+        
+
+
+    }
+}
