caf523c0caae09bc519546c1c83fbf1b2fa3c3c3
Max Liberzon <maxliberzon@gmail.com>
2018-02-11 12:28:39 -0800

    finished main program and started list class

--- maxshoppinglist/maxshoppinglist/Program.cs
+++ maxshoppinglist/maxshoppinglist/Program.cs
@@ -108,6 +108,7 @@ namespace maxshoppinglist
                 }
 
                 if (option == 2)
+
                 {
                     Console.WriteLine("");
                     Console.WriteLine("");
